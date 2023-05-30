ece0bfe0e6dde5fe7c60eeeb7638a05315c3b063
Marcus <marcusfoin1@gmail.com>
2023-05-30 18:37:53 +0200

    get all trie ok

--- DAO/TrajetDAO.php
+++ DAO/TrajetDAO.php
@@ -1,7 +1,11 @@
 <?php
+require_once("../Controleur/Distance.php");
 class TrajetDAO {
     public $courant;
     protected static $connect;
+    public function __construct($c=null){
+        $this->courant=$c;
+    }
     public static function setConnect($c){
         self::$connect = $c;
     }
@@ -23,7 +27,7 @@ class TrajetDAO {
     }
     public static function getAll($loc = null, $date = null, $fesId = null) {
         $query = "SELECT * FROM Trajet";
-    
+        /*
         // Vérifier si des filtres ont été spécifiés
         if ($date || $fesId) {
             $query .= " WHERE";
@@ -52,41 +56,63 @@ class TrajetDAO {
         if ($fesId) {
             $stmt->bindValue(':festival_id', $fesId);
         }
-    
+        */
         // Exécution de la requête
+        $stmt = self::$connect->prepare($query);
         $stmt->execute();
     
         // Récupération des résultats
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
     
         // Vérifier si une adresse de recherche est spécifiée
         if ($loc) {
             // Récupérer les coordonnées GPS de l'adresse de recherche
-            //$encodedAddress = urlencode($loc);
-            $url = "https://nominatim.openstreetmap.org/search?q=".$loc ."&format=json";
+            $url = "https://nominatim.openstreetmap.org/search?q=" . urlencode($loc) . "&format=json&email=marcusfoin2@gmail.com";
             $curl = curl_init($url);
-            curl_setopt($curl, CURLOPT_URL, $url);
+            $absCertPath = realpath("../Cert/ISRG Root X1.crt");
+            curl_setopt($curl, CURLOPT_CAINFO, $absCertPath);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
             $response = curl_exec($curl);
             curl_close($curl);
-            $data = json_decode($response);
-            if (!empty($data)){
-            $latitude = $data[0]['lat'];
-            $longitude = $data[0]['lon'];
-            // Calculer la distance entre l'adresse de recherche et chaque annonce
-            foreach ($results as &$result) {
-                $distance = distance($latitude, $longitude, $result['latitude'], $result['longitude']);
-                $result['distance'] = $distance;
-            }
+            $data = json_decode($response, true); // Ajoutez le deuxième argument `true` pour obtenir un tableau associatif
+            echo"a";
+            if (!empty($data)) {
+                echo"b";
+                $latitude = $data[0]['lat'];
+                $longitude = $data[0]['lon'];
+                echo $latitude."<br/>".$longitude;
+                // Calculer la distance entre l'adresse de recherche et chaque annonce
+                foreach ($results as &$result) {
+                    $url = "https://nominatim.openstreetmap.org/search?q=" . urlencode($result['localisation']) . "&format=json&email=marcusfoin2@gmail.com";
+                    $resCurl = curl_init($url);
+                    curl_setopt($resCurl, CURLOPT_CAINFO, $absCertPath);
+                    curl_setopt($resCurl, CURLOPT_RETURNTRANSFER, true); // Ajoutez cette option pour récupérer la réponse dans une variable
+                    $resResponse = curl_exec($resCurl);
+                    curl_close($resCurl);
+                    $resData = json_decode($resResponse, true); // Ajoutez le deuxième argument `true` pour obtenir un tableau associatif
+                    
+                if (!empty($resData)) {
+                    $resLatitude = $resData[0]['lat'];
+                    $resLongitude = $resData[0]['lon'];
+                        $distance = distance($latitude, $longitude, $resLatitude, $resLongitude);
+                        $result['distance'] = $distance;
+                        echo $distance."<br>";
+                    }}  
     
-            // Trier les résultats par ordre croissant de distance
-            usort($results, function ($a, $b) {
-                return $a['distance'] - $b['distance'];
-            });}
+                // Trier les résultats par ordre croissant de distance
+                usort($results, function ($a, $b) {
+                    return $a['distance'] - $b['distance'];
+                });
+            }{
+
+            }
         }
     
         // Retourner les résultats
         return $results;
     }
+    
     
     
 }
--- DAO/te.php
+++ DAO/te.php
@@ -1,5 +1,6 @@
 <?php
 require_once("TrajetDAO.php");
+//phpinfo();
 
 $dbFile = '../DB/Donne.db';
 $pdo = new PDO('sqlite:' . $dbFile);
@@ -11,8 +12,32 @@ echo "<table>";
     echo "<tr><th>loc</th></tr>";
     foreach ($as as $a) {
         echo "<tr>";
-        echo "<td>" . $a['Localisation'] . "</td>";
+        echo "<td>" . $a['localisation'] . "</td>";
         echo "</tr>";
     }
     echo "</table>";
+    /*
+    $url = "https://nominatim.openstreetmap.org/search?q=Paris&format=json&email=marcusfoin1@gmail.com";
+    $curl = curl_init($url);
+    $absCertPath = realpath("../Cert/ISRG Root X1.crt");
+    curl_setopt($curl, CURLOPT_CAINFO, $absCertPath);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); // Ajoutez cette option pour récupérer la réponse dans une variable
+    
+    $data = curl_exec($curl);
+    if ($data === false) {
+        var_dump(curl_error($curl));
+    } else {
+        $response = json_decode($data, true); // Ajoutez le deuxième argument `true` pour obtenir un tableau associatif
+    
+        if (isset($response[0])) { // Vérifiez si l'élément à l'indice 0 existe dans le tableau
+            $latitude = $response[0]['lat'];
+            $longitude = $response[0]['lon'];
+            echo $latitude."<br/>".$longitude;
+        } else {
+            echo "Aucune donnée de localisation trouvée.";
+        }
+    }
+    
+    curl_close($curl);
+    */
 ?>
