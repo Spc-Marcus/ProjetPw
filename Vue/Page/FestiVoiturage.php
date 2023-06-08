e7dae2e7a2c7e6c401d8128b417f6c847cbc1f03
Marcus <marcusfoin1@gmail.com>
2023-06-08 11:46:46 +0200

    save

---
+++ Vue/Page/FestiVoiturage.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>FestiVoiturage</title>
+    </head>
+    <body>
+        <?php include "../Template/menu.html"; ?>
+
+
+
+    </body>
+</html>
---
+++ Vue/Page/principalAdmin.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>FestiVoiturage Admin</title>
+</head>
+<body>
+    <?php include "../Template/menuAdmin.html";?>
+</body>
+</html>
